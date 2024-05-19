9ad5e6d5a70b1940b0f78ffb52ae8441ae0c1945
Fleischkuechle <zimmkon@yahoo.de>
2024-05-19 03:05:35 +0200

    added xcopy_exclude.txt

--- 2_Double_Click_Me_To_Create_Binarys_from_input_to_output.bat
+++ 2_Double_Click_Me_To_Create_Binarys_from_input_to_output.bat
@@ -31,6 +31,12 @@ set input=%cd%\input
 set output=%cd%\output
 REM copy  from  input to output folder
 xcopy "%input%\*" "%output%\" /s /e
+@REM REM this gives the ability to exlude subfolders from the copy process
+@REM @REM example in the xcopy_exclude.txt
+@REM @REM subfolder2\
+@REM @REM subfolder1\
+@REM @REM subfolder2\
+@REM xcopy "%input%\*" "%output%\" /s /e /exclude:xcopy_exclude.txt
 
 
 
